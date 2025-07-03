4ed85e1bf7df0b31e40d377ba51c8f21ac406d5b
Bunker91 <bunker91design@gmail.com>
2025-07-03 06:24:17 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,1 +1,23 @@
-// cabeçalho com login e menu
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import LogoutButton from './LogoutButton'
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between bg-[#330000] p-4 text-white shadow">
+      <Link href="/" className="text-xl font-bold">
+        Bunker91
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href="/kit" className="hover:underline">Kits</Link>
+        <Link href="/login" className="hover:underline">Entrar</Link>
+        <LogoutButton />
+      </div>
+    </header>
+  )
+}
+
+export default Header
